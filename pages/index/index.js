f098d7671633268b811fa50657f33ae60b5274bc
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-21 16:42:27 +0800

    顶部基本样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 // 首页逻辑区域
 import { getRecord } from "../../service/index/indexService.js"
-
+// 请求列表
 async function getList() {
     const res = await getRecord(12);
     console.log(res);
@@ -8,9 +8,5 @@ async function getList() {
 
 Page({
   data: {
-    
   },
-  onLoad() {
-    getList();
-  }
-})
+});
